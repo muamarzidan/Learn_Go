9814ed14f14012cb78c395eeb3a28719c3778294
muamarzidan <codeofomiru11@gmail.com>
2024-03-11 17:01:03 +0700

    fix: fix algoritma mengurutkan

--- Alpro/Procedure/mengurutkan.go
+++ Alpro/Procedure/mengurutkan.go
@@ -6,49 +6,54 @@ import "fmt"
 func main() {
 	var x, y int
 	fmt.Scan(&x, &y)
-	mengurutkan(&x, &y)
-}
 
-func mengurutkan(x, y *int) {
-	for *x != *y {
-		if *x > *y {
-			*x, *y = *y, *x
-		} else {
-			*x, *y = *x, *y
-		}
-		fmt.Println(*x, *y)
-		fmt.Scan(x, y)
+	for x != y {
+		mengurutkan(&x, &y)
+		fmt.Scan(&x, &y)
 	}
 }
 
+func mengurutkan(x, y *int) {
+	if *x > *y {
+		*x, *y = *y, *x
+	} else {
+		*x, *y = *x, *y
+	}
+	fmt.Println(*x, *y)
+}
 
-// Pseudocode
+
+
+// PSEUDOCODE
 
 // program main
 
-// kamus 
+// kamus
 // 	x, y : integer
 
 // algoritma
 // 	input(x, y)
-// 	mengurutkan(x, y)
+// 	while x != y do
+// 		mengurutkan(x, y)
+// 		input(x, y)
+// 	endwhile
 
 // endprogram
 
 
-// procedure mengurutkan(in/out x, y : integerx)
+// procedure mengurutkan(in/out x : integer, in/out y : integer)
+
+// {IS : initial state terdefinisi adalah x dan y dengan bilangan bulat positif}
+// {FS : final state terdefinisi adalah x dan y dengan bilangan bulat positif}
 
 // kamus
 
 // algoritma
-// 	for x != y
-// 		if x > y then
-// 			x, y <- y, x
-// 		else
-// 			x, y <- x, y
-// 		endif
-// 		output(x, y)
-// 		input(x, y)
-// 	endfor
+// 	if x > y then
+// 		x, y <- y, x
+// 	else
+// 		x, y <- x, y
+// 	endif
+// 	output(x, y)
 
 // endprocedure
