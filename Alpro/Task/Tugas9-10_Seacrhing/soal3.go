189e4e650d688b2e59730bb67b149c9c3ad1e28d
muamarzidan <codeofomiru11@gmail.com>
2024-05-26 18:07:52 +0700

    updt: Task Week 9-10

--- Alpro/Task/Tugas9-10_Seacrhing/soal3.go
+++ Alpro/Task/Tugas9-10_Seacrhing/soal3.go
@@ -52,3 +52,81 @@ func cetakarr(himpunanA [MAX]int, ukuran int) {
 }
 
 
+
+// PSEUDOCODE
+
+// constant MAX = 100
+
+// program main 
+
+// kamus 
+// 	himpunanA : array of integer
+// 	himpunanB : array of integer
+// 	lenA : integer
+// 	lenB : integer
+// 	himpunanC : array of integer
+// 	lenC : integer
+
+// algoritma
+// 	himpunanA <- [11, 28, 33, 64, 95, 16, 100, 15]
+// 	himpunanB <- [3, 11, 7, 28, 33, 6]
+// 	lenA <- 8
+// 	lenB <- 6
+
+// 	himpunanC <- gabungan(himpunanA, himpunanB, lenA, lenB)
+// 	cetakarr(himpunanC, lenC)
+
+// endprogram
+
+
+// function gabungan(himpunanA, himpunanB, lenA, lenB) -> integer
+
+// kamus 
+// 	totalC : integer
+// 	i : integer
+
+// algoritma
+// 	totalC <- 0
+// 	for i <- 0 to lenA - 1
+// 		himpunanC[totalC] <- himpunanA[i]
+// 		totalC <- totalC + 1
+// 	endfor
+// 	for i <- 0 to lenB - 1
+// 		if not isiarr(himpunanB[i], himpunanC, totalC) then
+// 			himpunanC[totalC] <- himpunanB[i]
+// 			totalC <- totalC + 1
+// 		endif	
+// 	endfor
+// 	return totalC
+// endfunction
+
+
+// function isiarr(isi, array, ukuran) -> boolean
+
+// kamus
+// 	i : integer
+
+// algoritma
+// 	for i <- 0 to ukuran - 1
+// 		if array[i] = isi then
+// 			return true
+// 		endif
+// 	endfor
+// 	return false
+
+// endfunction
+
+
+// procedure cetakarr(himpunanA, ukuran)
+
+// kamus
+// 	i : integer
+
+// algoritma
+// 	for i <- 0 to ukuran - 1
+// 		if i > 0 then
+// 			output(", ")
+// 		endif
+// 		output(himpunanA[i])
+// 	endfor
+// endprocedure
