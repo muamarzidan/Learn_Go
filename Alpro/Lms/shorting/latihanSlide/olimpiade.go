f29ce2df76671e6fb5a4602016c5d9e44e581a37
muamarzidan <codeofomiru11@gmail.com>
2024-05-29 22:08:13 +0700

    updt: LMS Shorting latihanSlide

--- Alpro/Lms/shorting/latihanSlide/olimpiade.go
+++ Alpro/Lms/shorting/latihanSlide/olimpiade.go
@@ -55,3 +55,98 @@ func sorting(t *olimpiade, n int) {
 		pass = pass + 1
 	}
 }
+
+
+PSEUDOCODE
+
+type peserta struct <
+	nama: string
+	g: integer
+	s: integer
+	b: integer
+>
+
+type olimpiade: array[1..100] of peserta
+
+
+program main
+
+kamus 
+	tab: olimpiade
+	n: integer
+
+algoritma
+	isiArray(tab, n)
+	sorting(tab, n)
+	tampilArray(tab, n)
+
+endprogram
+
+
+
+procedure isiArray(t : olimpiade, n: integer)
+
+kamus
+	i: integer
+
+algoritma
+	input(n)
+	for i <- 0 to n-1
+		input(t[i].nama, t[i].g, t[i].s, t[i].b)
+	endfor
+
+endprocedure
+
+
+
+procedure tampilArray(t: olimpiade, n: integer)
+
+kamus
+	i: integer
+
+algoritma
+	for i <- 0 to n-1
+		output(t[i].nama, t[i].g, t[i].s, t[i].b)
+	endfor
+
+endprocedure
+
+
+
+function poin(g, s, b : integer) -> integer
+
+kamus
+	hasil: integer
+
+algoritma
+	hasil <- (4 * g) + (3 * s) + b
+	return hasil
+
+endfunction
+
+
+
+procedure sorting(t: olimpiade, n: integer)
+
+kamus
+	pass, idx, i: integer
+	temp: peserta
+
+algoritma
+	pass <- 1
+	while pass <= n-1
+		idx <- pass - 1
+		i <- pass
+		while i < n
+			if poin(t[idx].g, t[idx].s, t[idx].b) < poin(t[i].g, t[i].s, t[i].b) then
+				idx <- i
+			endif
+			i <- i + 1
+		endwhile
+		temp <- t[pass-1]
+		t[pass-1] <- t[idx]
+		t[idx] <- temp
+		pass <- pass + 1
+	endwhile
+	
+endprocedure
