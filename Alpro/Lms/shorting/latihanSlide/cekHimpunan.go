3016d922ee21f90e618fc867568ce89e352745d8
muamarzidan <codeofomiru11@gmail.com>
2024-05-30 22:49:03 +0700

    updt: LMS Shorting latihanSlide

--- Alpro/Lms/shorting/latihanSlide/cekHimpunan.go
+++ Alpro/Lms/shorting/latihanSlide/cekHimpunan.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 const nMax = 37
-const N = 5
 
 type tHimpunan struct {
 	anggota [nMax]int
@@ -13,7 +12,7 @@ type tHimpunan struct {
 func main() {
 	var himp1, himp2 tHimpunan
 	var n int 
-	fmt.Println("masukan batas jumlah anggota himpunan maximal")
+	fmt.Println("Masukkan batas jumlah anggota himpunan maksimal:")
 	fmt.Scan(&n)
 	bacaMasukan(&himp1, n)
 	bacaMasukan(&himp2, n)
@@ -27,15 +26,20 @@ func main() {
 }
 
 func bacaMasukan(set *tHimpunan, n int) {
-	set.panjang = n
+	set.panjang = 0
 	fmt.Println("Masukkan anggota himpunan:")
-	for i := 1; i <= n; i++ {
-		fmt.Scan(&set.anggota[i])
+	for i := 0; i < n; i++ {
+		var x int
+		fmt.Scan(&x)
+		if !ada(*set, x) { // hanya tambahkan jika tidak ada
+			set.anggota[set.panjang] = x
+			set.panjang++
+		}
 	}
 }
 
 func ada(set tHimpunan, x int) bool {
-	for i := 1; i <= set.panjang; i++ {
+	for i := 0; i < set.panjang; i++ {
 		if set.anggota[i] == x {
 			return true
 		}
@@ -44,14 +48,17 @@ func ada(set tHimpunan, x int) bool {
 }
 
 func urut(set *tHimpunan) {
-	for i := 2; i <= set.panjang; i++ {
-		temp := set.anggota[i]
-		j := i - 1
-		for j >= 1 && set.anggota[j] > temp {
-			set.anggota[j+1] = set.anggota[j]
-			j--
+	var pass, idx, temp, i int
+	for pass = 0; pass < set.panjang-1; pass++ {
+		idx = pass
+		for i = pass + 1; i < set.panjang; i++ {
+			if set.anggota[idx] < set.anggota[i] {
+				idx = i
+			}
 		}
-		set.anggota[j+1] = temp
+		temp = set.anggota[pass]
+		set.anggota[pass] = set.anggota[idx]
+		set.anggota[idx] = temp
 	}
 }
 
@@ -59,7 +66,7 @@ func sama(set1, set2 tHimpunan) bool {
 	if set1.panjang != set2.panjang {
 		return false
 	}
-	for i := 1; i <= set1.panjang; i++ {
+	for i := 0; i < set1.panjang; i++ {
 		if set1.anggota[i] != set2.anggota[i] {
 			return false
 		}
