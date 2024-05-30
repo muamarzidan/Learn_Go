f7c3905597d1f6db1b4a2315da0d240d60e7fa35
muamarzidan <codeofomiru11@gmail.com>
2024-05-30 23:21:34 +0700

    updt: LMS Shorting latihanSlide

--- Alpro/Lms/shorting/latihanSlide/bunga.go
+++ Alpro/Lms/shorting/latihanSlide/bunga.go
@@ -53,3 +53,88 @@ func tampilArray(tabBunga arrayBunga, N int) {
 		fmt.Println(tabBunga[i])
 	}
 }
+
+
+
+PSEUDOCODE
+
+constant nMax = 1000
+
+type arrayBunga array[1..nMax] of string
+
+program main
+
+kamus 
+	tabBunga : arrayBunga
+	N : integer
+
+algoritma
+	isiArray(tabBunga, N)
+	mengurutkan(tabBunga, N)
+	tampilArray(tabBunga, N)
+
+endprogram
+
+
+procedure mengurutkan(var tabBunga : arrayBunga, N : integer)
+
+kamus 
+	pass, idx, i : integer
+	temp : string
+
+algoritma
+	pass <- 1
+	while pass <= N-1 do
+		idx <- pass - 1
+		i <- pass
+		while i < N do
+			if panjang(tabBunga[idx]) > panjang(tabBunga[i]) then
+				idx <- i
+			endif
+			i <- i + 1
+		endwhile
+		temp <- tabBunga[pass-1]
+		tabBunga[pass-1] <- tabBunga[idx]
+		tabBunga[idx] <- temp
+		pass <- pass + 1
+	endwhile
+
+endprocedure
+
+
+function panjang(bunga : string) -> integer
+
+kamus
+	-
+
+algoritma
+	return len(bunga) - 2
+
+endfunction
+
+
+procedure isiArray(var tabBunga : arrayBunga, var N : integer)
+
+kamus
+	i : integer
+
+algoritma
+	input(N)
+	for i <- 0 to N-1 do
+		input(tabBunga[i])
+	endfor
+
+endprocedure
+
+
+procedure tampilArray(tabBunga : arrayBunga, N : integer)	
+
+kamus
+	i : integer
+
+algoritma
+	for i <- 0 to N-1 do
+		output(tabBunga[i])
+	endfor
+	
+endprocedure
